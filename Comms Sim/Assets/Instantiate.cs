e2a56209d442ceeb238307471ec9ee10e42ee26c
LeoniSayat <l30.sayat@gmail.com>
2023-05-26 04:30:36 -0700

    LineRenderer

--- Comms Sim/Assets/Instantiate.cs
+++ Comms Sim/Assets/Instantiate.cs
@@ -5,9 +5,10 @@ using UnityEngine;
 public class Instantiate : MonoBehaviour
 {
     // Start is called before the first frame update
+    public GameObject Earth;
     void Start()
     {
-        
+        Instantiate(Earth, new Vector3(0,0,0), Quaternion.identity);
     }
 
     // Update is called once per frame

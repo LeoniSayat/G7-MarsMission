d346d6a7db44373eebfdde5ba22066d67a48df04
LeoniSayat <l30.sayat@gmail.com>
2023-05-27 05:24:29 -0700

    Complete enough..

--- Comms Sim/Assets/Scripts/UpdateUI.cs
+++ Comms Sim/Assets/Scripts/UpdateUI.cs
@@ -8,6 +8,7 @@ public class UpdateUI : MonoBehaviour
 
     [SerializeField] TMPro.TextMeshProUGUI textUI;
     public DrawLine lineRenderer;
+    int count = 0;
 
     // Start is called before the first frame update
     void Start()
@@ -21,6 +22,7 @@ public class UpdateUI : MonoBehaviour
         double dist = lineRenderer.getDistance() * 10000.0;
         String time  = (LineCollision.askSend()) ? System.String.Format("{0:00000.000}", (dist / (299792.458))) : "N/A";
         textUI.text = System.String.Format("Distance: {0:00000.000} km\nEst. Arrival {1} seconds", dist, time);
+        // textUI.text = System.String.Format("Distance: {0:00000.000} km\nEst. Arrival {1} seconds\nCurrent runtime: {2}", dist, time, ++count);
         // textUI.text = "Distance: " + dist + "\nEst. Arrival: " + time;
         //Debug.Log("Distance: " + dist + "\nEst. Arrival: " + time);
 
